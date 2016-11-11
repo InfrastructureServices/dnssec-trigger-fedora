de21328de9d9453d8af20f177eecace0c9c6f8a8
Martin Sehnoutka <msehnout@redhat.com>
2016-11-11 17:40:13 +0100

    Merge master. Beggining of yield.. function.

--- riggerd/fwd_zones.c
+++ riggerd/fwd_zones.c
@@ -11,9 +11,27 @@ struct nm_connection_list yield_connections_from_json(char *json)
     nm_connection_list_init(&ret);
 
     if (json_validate(json) == true) {
-        printf("I've got valid json and it looks like this:\n%s\n", json);
+	printf("I've got valid json and it looks like this:\n%s\n", json);
+
+	/* Decode the input string and check it again */
+	JsonNode *head = json_decode(json);
+	//shouldn't be null because we've checked its validity
+	if (head->tag != JSON_OBJECT) {
+	    return ret;
+
+	}
+
+	/* We expect to get a list of connections. Anything else is not valid input,
+	 * even though it might be valid json. */
+	JsonNode *node = head->children.head; // now it should be the first dictionary value i.e. connections
+	if (NULL == node || strncmp(node->key, "connections", 11) != 0) { // and also must be an array
+	    return ret;
+	}
+	// node is array called connections
+
     } else {
-        printf("Invalid json input");
+	printf("Invalid json input\n");
+        // TODO: log error message into syslog
     }
 
     return ret;
